6703ba10c48a3a941abd8513bba3cd5a4193b06b
Michael Pocta <michaelpocta@Michaels-MacBook-Air.local>
2021-06-17 22:08:54 -0400

    Updates

--- component---src-pages-page-2-js-73ee9029bcbd04ba1bcc.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[617],{9863:function(e,t,a){"use strict";a.r(t);var l=a(7294),n=a(5444),r=a(3088),c=a(3751);t.default=function(){return l.createElement(r.Z,null,l.createElement(c.Z,{title:"Page two"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-73ee9029bcbd04ba1bcc.js.map
