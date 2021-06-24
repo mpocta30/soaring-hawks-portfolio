3787cb87762ffa784ca025a5c4d0d0d00ea97a32
mpocta30 <michaelpocta@yahoo.com>
2021-06-23 21:23:39 -0400

    Updates

---
+++ 31664189-f10b67c51b46257c974a.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[609],{9295:function(t,a,e){"use strict";e.d(a,{opg:function(){return s}});var r=e(1046);function s(t){return(0,r.w_)({tag:"svg",attr:{version:"1.1",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M8 0c-2.761 0-5 2.239-5 5 0 5 5 11 5 11s5-6 5-11c0-2.761-2.239-5-5-5zM8 8c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"}}]})(t)}}}]);
+//# sourceMappingURL=31664189-f10b67c51b46257c974a.js.map
