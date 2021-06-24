6363304e6c941e2f1dbdc85ddc42d9c71c21b163
mpocta30 <michaelpocta@yahoo.com>
2021-06-23 23:28:29 -0400

    changed space id to env variable

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
     {
       resolve: "gatsby-source-contentful",
       options: {
-        spaceId: "ozl916uy4jos",
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
